93205deebd36224d29ebe62750ce5a1fdf1c15c4
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2025-02-14 15:42:07 +0000

    Add mongo url and connect to db

--- Project_2/config/env.js
+++ Project_2/config/env.js
@@ -3,4 +3,4 @@ import { config } from "dotenv";
 const envFile = `.env.${process.env.NODE_ENV || "development"}.local`;
 config({ path: envFile });
 
-export const { PORT, NODE_ENV } = process.env;
+export const { PORT, NODE_ENV, MONGO_URI } = process.env;
