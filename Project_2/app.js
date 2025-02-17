7988acdf802f01b39217bb59ce83d94a30ad88b2
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2025-02-17 14:33:46 +0000

    Add workflow logic

--- Project_2/app.js
+++ Project_2/app.js
@@ -8,6 +8,7 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/workflows", workflowRouter);
 
 app.use(errorMiddleware);
 app.use(arcjetMiddleware);
--- Project_2/config/upstash.js
+++ Project_2/config/upstash.js
@@ -2,7 +2,7 @@ import { Client as WorkflowClient } from "@upstash/workflow";
 import {
   QSTASH_TOKEN,
   QSTASH_URL
-} from "./env";
+} from "./env.js";
 
 export const workflowClient = new WorkflowClient({
   baseUrl: QSTASH_URL,
